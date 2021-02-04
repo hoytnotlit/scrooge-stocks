ddc01c63b56515c65d8baf77cfb2e575094047dc
minerva <minerva.suvanto@gmail.com>
2021-02-04 13:20:07 +0100

    some more refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,9 @@ export class AppComponent {
 
   stocks = new Array<Stock>();
   stocksFiltered = new Array<Stock>();
-  stocksPriceChange = new Array<Stock>();
-  stocksBestOpening = new Array<Stock>();
-  longestUpwardDays: number;
+  // stocksPriceChange = new Array<Stock>();
+  // stocksBestOpening = new Array<Stock>();
+  // longestUpwardDays: number;
 
   from: Date;
   to: Date;
@@ -61,96 +61,71 @@ export class AppComponent {
     this.stocksFiltered = this.helperService.filterByDate(this.stocks, this.from, this.to);
 
     // get answers for questions
-    this.longestUpwardDays = this.getLongestUpwardDays();
-    this.stocksPriceChange = this.getStockPriceChange();
-    this.stocksBestOpening = this.getBestOpeningPrice();
+    // this.longestUpwardDays = this.getLongestUpwardDays();
+    // this.stocksPriceChange = this.getStockPriceChange();
+    // this.stocksBestOpening = this.getBestOpeningPrice();
   }
 
-  getLongestUpwardDays(): number {
-    let maxDays = 0;
-    let stocksSorted = this.helperService.sortByDateAscending(this.stocksFiltered);
+  // getLongestUpwardDays(): number {
+  //   let maxDays = 0;
+  //   let stocksSorted = this.helperService.sortByDateAscending(this.stocksFiltered);
 
-    // TODO remove repetition
-    for (let i = 0; i < stocksSorted.length; i++) {
-      let nextDayIndex = i + 1;
-      let prevDayPrice = stocksSorted[i].closeLast;
-      let days = 1;
+  //   // TODO remove repetition
+  //   for (let i = 0; i < stocksSorted.length; i++) {
+  //     let nextDayIndex = i + 1;
+  //     let prevDayPrice = stocksSorted[i].closeLast;
+  //     let days = 1;
 
-      while (nextDayIndex < stocksSorted.length && prevDayPrice < stocksSorted[nextDayIndex].closeLast) {
-        // TODO scen. price 1st day: 12, 2nd day: 15, 3rd: 14 -> updward trend is 2 days?
-        prevDayPrice = stocksSorted[nextDayIndex].closeLast;
-        days++;
-        nextDayIndex++;
-      }
+  //     while (nextDayIndex < stocksSorted.length && prevDayPrice < stocksSorted[nextDayIndex].closeLast) {
+  //       // TODO scen. price 1st day: 12, 2nd day: 15, 3rd: 14 -> updward trend is 2 days?
+  //       prevDayPrice = stocksSorted[nextDayIndex].closeLast;
+  //       days++;
+  //       nextDayIndex++;
+  //     }
 
-      maxDays = days > maxDays ? days : maxDays;
-    }
+  //     maxDays = days > maxDays ? days : maxDays;
+  //   }
 
-    return maxDays;
-  }
-
-  getStockPriceChange(): Stock[] {
-    let result = new Array<Stock>();
-
-    for (let s of this.stocksFiltered) {
-      // calculate the stock price change within a day
-      s.priceChange = s.high - s.low;
-      result.push(s)
-    }
-
-    // The list is ordered by
-    // volume and price change. So if two dates have the same volume, the one with the
-    // more significant price change should come first.
-
-    // sort by volume and price change, prioritizing volume
-    result.sort((a, b) => { return a.volume - b.volume || a.priceChange - b.priceChange; });
-    return result;
-  }
-
-  getBestOpeningPrice(): Stock[] {
-    let result = new Array<Stock>();
-    const days = 5;
-
-    for (let s of this.stocksFiltered) {
-      let n = this.stocks.indexOf(s); // index of current day
-      // order of stocks is descending -> get closing prices of next 5 items of current day
-      let A = this.stocks.slice(n + 1, n + (days + 1)).map(s => s.closeLast);
-
-      // calculate sma and difference between opening price and sma
-      if (A.length == days) {
-        let sma = A.reduce((a, b) => a + b) / days;
-        let priceChangePercentage = (s.open - sma) / s.open;
-        s.smaPriceChangePercentage = priceChangePercentage;
-        result.push(s);
-      }
-    }
-
-    // sort by price change percentage
-    return result.sort((a, b) => { return b.smaPriceChangePercentage - a.smaPriceChangePercentage });
-  }
-
-
-  // TODO helper service
-
-  // // filter by given date range
-  // filterByDate(data: Stock[]): Stock[] {
-  //   //TODO these times are +1 when selected from the datepicker
-  //   // console.log(new Date(this.from))
-  //   // console.log(new Date(this.to))
-  //   return data.filter(s => {
-  //     return new Date(s.date) >= new Date(this.from)
-  //       && new Date(s.date) <= new Date(this.to)
-  //   });
+  //   return maxDays;
   // }
 
-  // sortByDateAscending(data: Stock[]): Stock[] {
-  //   return data.sort((a, b) => {
-  //     return new Date(a.date).getTime() - new Date(b.date).getTime()
-  //   })
+  // getStockPriceChange(): Stock[] {
+  //   let result = new Array<Stock>();
+
+  //   for (let s of this.stocksFiltered) {
+  //     // calculate the stock price change within a day
+  //     s.priceChange = s.high - s.low;
+  //     result.push(s)
+  //   }
+
+  //   // The list is ordered by
+  //   // volume and price change. So if two dates have the same volume, the one with the
+  //   // more significant price change should come first.
+
+  //   // sort by volume and price change, prioritizing volume
+  //   result.sort((a, b) => { return a.volume - b.volume || a.priceChange - b.priceChange; });
+  //   return result;
   // }
 
-  // // convert price from string to float
-  // getPrice(price: string): number {
-  //   return parseFloat(price.trim().replace("$", ""));
+  // getBestOpeningPrice(): Stock[] {
+  //   let result = new Array<Stock>();
+  //   const days = 5;
+
+  //   for (let s of this.stocksFiltered) {
+  //     let n = this.stocks.indexOf(s); // index of current day
+  //     // order of stocks is descending -> get closing prices of next 5 items of current day
+  //     let A = this.stocks.slice(n + 1, n + (days + 1)).map(s => s.closeLast);
+
+  //     // calculate sma and difference between opening price and sma
+  //     if (A.length == days) {
+  //       let sma = A.reduce((a, b) => a + b) / days;
+  //       let priceChangePercentage = (s.open - sma) / s.open;
+  //       s.smaPriceChangePercentage = priceChangePercentage;
+  //       result.push(s);
+  //     }
+  //   }
+
+  //   // sort by price change percentage
+  //   return result.sort((a, b) => { return b.smaPriceChangePercentage - a.smaPriceChangePercentage });
   // }
 }
--- src/app/components/stock/stock.component.ts
+++ src/app/components/stock/stock.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Stock } from 'src/app/models/stock.model';
+import { HelperService } from 'src/app/services/helper.service';
 
 @Component({
   selector: 'app-stock',
@@ -7,73 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockComponent implements OnInit {
 
-  constructor() { }
+  @Input() stocks: Stock[];
+  @Input() stocksFiltered: Stock[];
+  stocksPriceChange = new Array<Stock>();
+  stocksBestOpening = new Array<Stock>();
+  longestUpwardDays: number;
+
+  from: Date;
+  to: Date;
+
+  constructor(private helperService: HelperService) { }
 
   ngOnInit() {
   }
 
+  ngOnChanges() {
+    this.longestUpwardDays = this.getLongestUpwardDays();
+    this.stocksPriceChange = this.getStockPriceChange();
+    this.stocksBestOpening = this.getBestOpeningPrice();
+  }
 
-  // getLongestUpwardDays(): number {
-  //   let maxDays = 0;
-  //   let stocksSorted = this.sortByDateAscending(this.stocksFiltered);
+  getLongestUpwardDays(): number {
+    let maxDays = 0;
+    let stocksSorted = this.helperService.sortByDateAscending(this.stocksFiltered);
 
-  //   // TODO remove repetition
-  //   for (let i = 0; i < stocksSorted.length; i++) {
-  //     let nextDayIndex = i + 1;
-  //     let prevDayPrice = stocksSorted[i].closeLast;
-  //     let days = 1;
+    // TODO remove repetition
+    for (let i = 0; i < stocksSorted.length; i++) {
+      let nextDayIndex = i + 1;
+      let prevDayPrice = stocksSorted[i].closeLast;
+      let days = 1;
 
-  //     while (nextDayIndex < stocksSorted.length && prevDayPrice < stocksSorted[nextDayIndex].closeLast) {
-  //       // TODO scen. price 1st day: 12, 2nd day: 15, 3rd: 14 -> updward trend is 2 days?
-  //       prevDayPrice = stocksSorted[nextDayIndex].closeLast;
-  //       days++;
-  //       nextDayIndex++;
-  //     }
+      while (nextDayIndex < stocksSorted.length && prevDayPrice < stocksSorted[nextDayIndex].closeLast) {
+        // TODO scen. price 1st day: 12, 2nd day: 15, 3rd: 14 -> updward trend is 2 days?
+        prevDayPrice = stocksSorted[nextDayIndex].closeLast;
+        days++;
+        nextDayIndex++;
+      }
 
-  //     maxDays = days > maxDays ? days : maxDays;
-  //   }
+      maxDays = days > maxDays ? days : maxDays;
+    }
 
-  //   return maxDays;
-  // }
+    return maxDays;
+  }
 
-  // getStockPriceChange(): Stock[] {
-  //   let result = new Array<Stock>();
+  getStockPriceChange(): Stock[] {
+    let result = new Array<Stock>();
 
-  //   for (let s of this.stocksFiltered) {
-  //     // calculate the stock price change within a day
-  //     s.priceChange = s.high - s.low;
-  //     result.push(s)
-  //   }
+    for (let s of this.stocksFiltered) {
+      // calculate the stock price change within a day
+      s.priceChange = s.high - s.low;
+      result.push(s)
+    }
 
-  //   // The list is ordered by
-  //   // volume and price change. So if two dates have the same volume, the one with the
-  //   // more significant price change should come first.
+    // The list is ordered by
+    // volume and price change. So if two dates have the same volume, the one with the
+    // more significant price change should come first.
 
-  //   // sort by volume and price change, prioritizing volume
-  //   result.sort((a, b) => { return a.volume - b.volume || a.priceChange - b.priceChange; });
-  //   return result;
-  // }
+    // sort by volume and price change, prioritizing volume
+    result.sort((a, b) => { return a.volume - b.volume || a.priceChange - b.priceChange; });
+    return result;
+  }
 
-  // getBestOpeningPrice(): Stock[] {
-  //   let result = new Array<Stock>();
-  //   const days = 5;
+  getBestOpeningPrice(): Stock[] {
+    let result = new Array<Stock>();
+    const days = 5;
 
-  //   for (let s of this.stocksFiltered) {
-  //     let n = this.stocks.indexOf(s); // index of current day
-  //     // order of stocks is descending -> get closing prices of next 5 items of current day
-  //     let A = this.stocks.slice(n + 1, n + (days + 1)).map(s => s.closeLast);
+    for (let s of this.stocksFiltered) {
+      let n = this.stocks.indexOf(s); // index of current day
+      // order of stocks is descending -> get closing prices of next 5 items of current day
+      let A = this.stocks.slice(n + 1, n + (days + 1)).map(s => s.closeLast);
 
-  //     // calculate sma and difference between opening price and sma
-  //     if (A.length == days) {
-  //       let sma = A.reduce((a, b) => a + b) / days;
-  //       let priceChangePercentage = (s.open - sma) / s.open;
-  //       s.smaPriceChangePercentage = priceChangePercentage;
-  //       result.push(s);
-  //     }
-  //   }
+      // calculate sma and difference between opening price and sma
+      if (A.length == days) {
+        let sma = A.reduce((a, b) => a + b) / days;
+        let priceChangePercentage = (s.open - sma) / s.open;
+        s.smaPriceChangePercentage = priceChangePercentage;
+        result.push(s);
+      }
+    }
 
-  //   // sort by price change percentage
-  //   return result.sort((a, b) => { return b.smaPriceChangePercentage - a.smaPriceChangePercentage });
-  // }
+    // sort by price change percentage
+    return result.sort((a, b) => { return b.smaPriceChangePercentage - a.smaPriceChangePercentage });
+  }
 
 }
